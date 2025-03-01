7e35fbf5e0937e7a783cd91c33b36cf28a0d31fb
florisvanamersf <38800747-florisvanamersf@users.noreply.replit.com>
2025-03-01 12:25:54 +0000

    Restored to 'ed230894b0f01913caace60eb9cd25b2bc4d9ecc'
    Replit-Restored-To: ed230894b0f01913caace60eb9cd25b2bc4d9ecc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,8 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "@/hooks/use-theme";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
-import { AccessibilityBar } from "@/components/accessibility-bar";
 
 function Router() {
   return (
@@ -19,11 +17,8 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <AccessibilityBar />
-        <Router />
-        <Toaster />
-      </ThemeProvider>
+      <Router />
+      <Toaster />
     </QueryClientProvider>
   );
 }
